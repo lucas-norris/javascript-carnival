a59fef70e70cc9baba98aea77d3f68f3c02215bf
Lucas Norris <luke.norris22@gmail.com>
2022-05-13 10:02:36 +1200

    Finish inflate the unicorn challenge

--- inflate-the-unicorn/inflate-the-unicorn.js
+++ inflate-the-unicorn/inflate-the-unicorn.js
@@ -1,5 +1,26 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Inflate The Unicorn!")
+console.log('Inflate The Unicorn!')
+
+let unicornImg = document.getElementsByClassName('inflate-an-image')
+
+for (let i = 0; i < unicornImg.length; i++) {
+  unicornImg[i].onclick = inflate
+}
+
+//write function and call it with onclick method - gets called every time you click on a unicorn
+
+function inflate(e) {
+  let unicorn = e.target
+  if (unicorn.src.includes('images/unicorn-0.png')) {
+    unicorn.src = './images/unicorn-1.png'
+  } else if (unicorn.src.includes('images/unicorn-1.png')) {
+    unicorn.src = './images/unicorn-2.png'
+  } else if (unicorn.src.includes('images/unicorn-2.png')) {
+    unicorn.src = './images/unicorn-3.png'
+  } else {
+    alert('Unicorn ' + unicorn.id + ' says thank you')
+  }
+}
